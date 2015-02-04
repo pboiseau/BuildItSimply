786b88d2a07ad1023629766b177f8611e0968f7e
Mikaoo <vong.bernard@gmail.com>
2015-02-04 18:50:51 +0100

    REGEX entropy verification

--- app/models/Validate.php
+++ app/models/Validate.php
@@ -7,23 +7,23 @@ class Validate extends Audit
 	{
 		$check = preg_match("#^[a-zA-Z ]{1,}$#", $data);
 		$length = (!empty($length)) ? (strlen($data) <= $length) : true;
-		return ($check == 1) && ($length);
+		return (($check == 1) && $length);
 	}
 
 	public function isNumber($data, $length = null)
 	{
 		$check = preg_match("#^[0-9]{1,}$#", $data);
 		$length = (!empty($length)) ? (strlen($data) <= $length) : true;
-		return ($check == 1) && ($length);
+		return (($check == 1) && $length);
 	}
 
 
 	public function isPassword($data, $entropy = null, $length = null)
 	{
-
+		$strong = (!empty($strong) && ($this->entropy($data) > $entropy));
 		$check = preg_match("#^[^ ;'\"]{8,25}#", $data);
 		$length = (!empty($length)) ? (strlen($data) <= $length) : true;
-		return ($check == 1) && ($length) && ($this->entropy($data) > $entropy);
+		return (($check == 1) && $length && $strong);
 	}
 
 
