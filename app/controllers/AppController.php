8d9b483b23099df9c83c4dfc7becd50a97f10fce
robinMastro <robin.mastromarino@gmail.com>
2015-02-20 11:43:40 +0100

    fix image link

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -26,7 +26,7 @@ class AppController
             'webroot' => $this->f3->get('WEBROOT'),
             'css' => $this->f3->get('CSS'),
             'js' => $this->f3->get('JS'),
-            'image' => $this->f3->get('IMAGE'),
+            'image' => ($this->f3->get('PROD')) ? $this->f3->get('ROOT') . $this->f3->get('IMAGE') : $this->f3->get('DEV_ROOT') . '/' .$this->f3->get('IMAGE'),
             'request' => substr($this->f3->get('PATTERN'), 1, strlen($this->f3->get('PATTERN'))),
             'message' => $this->f3->get('SESSION.message'),
             'login' => $this->f3->get('SESSION.user'),
